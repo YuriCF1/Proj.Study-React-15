5211699a3b1fad0d080b19cc667a9197db2e334b
Yuri Cruz <yuricruzf@gmail.com>
2023-08-09 13:06:36 -0300

    Implementing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 //Interfaces
-import { ITask } from "./interfaces/Task";
-
-import TaskForm from "./components/TaskForm";
-import TaskList from "./components/TaskList";
+import { ITask } from "./interfaces/ITask";
 
 import "./App.css";
 
 //Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
 //Style
 import styles from "./App.module.css";
 
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+
   return (
     <>
       <Header />
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText="Criar Tarefa" />
+          <TaskForm btnText="Criar Tarefa" taskList={taskList} setTaskList={setTaskList}/>
         </div>
         <div>
           <h2>Suas tarefas</h2>
