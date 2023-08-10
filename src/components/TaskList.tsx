fcd77cbc97b9fdc00cf4543bf59e7d5e5f639468
Yuri Cruz <yuricruzf@gmail.com>
2023-08-09 22:54:35 -0300

    Elaborating html of list

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 
-type Props = {};
+//Interfaces
+import { ITask } from "../interfaces/ITask";
 
-const TaskList = (props: Props) => {
+//CSS
+import styles from "./TaskList.module.css";
+
+type Props = {
+  taskList: ITask[];
+};
+
+const TaskList = ({ taskList }: Props) => {
   return (
-    <div>
-      <p>Lista tarefas:</p>
-    </div>
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => {
+          return (
+            <div key={task.id}>
+              <div>
+                <h4>{task.title}</h4>
+                <p>Dificuldade: {task.difficulty}</p>
+              </div>
+              <div>
+                <i className="bi bi-pencil"></i>
+                <i className="bi bi-trash"></i>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <p>Não há tarefas</p>
+      )}
+    </>
   );
 };
 
