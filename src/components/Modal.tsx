8f19a1a19f9649fc44ea46a6f8e7f4ef9ab4dc5d
Yuri Cruz <yuricruzf@gmail.com>
2023-08-13 23:02:51 -0300

    Using children on modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,14 +2,17 @@ import React from "react";
 
 import style from "./Modal.module.css";
 
-type Props = {};
+type Props = {
+  children: React.ReactNode;
+};
 
-const Modal = (props: Props) => {
+const Modal = ({ children }: Props) => {
   return (
     <div id="modal">
       <div className={style.fade}></div>
       <div className={style.modal}>
         <h2>Texto modal</h2>
+        {children}
       </div>
     </div>
   );
