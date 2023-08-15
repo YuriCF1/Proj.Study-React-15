c26317875e9c12c5a82451a8f334217a29a0146f
Yuri Cruz <yuricruzf@gmail.com>
2023-08-14 21:29:32 -0300

    Closing modal by the X icon

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,11 +7,21 @@ type Props = {
 };
 
 const Modal = ({ children }: Props) => {
+  const closeModal = (e: React.MouseEvent): void => {
+    console.log("Clicado");
+    const xClose = document.querySelector("#xClose");
+    console.log(xClose);
+    xClose!.parentElement!.parentElement!.parentElement!.classList.add("hide");
+  };
+
   return (
     <div id="modal">
       <div className={style.fade}></div>
       <div className={style.modal}>
-        <h2>Texto modal</h2>
+        <div className={style.headerModal}>
+          <h2>Texto modal</h2>
+          <i id="xClose" className="bi bi-x" onClick={closeModal}></i>
+        </div>
         {children}
       </div>
     </div>
